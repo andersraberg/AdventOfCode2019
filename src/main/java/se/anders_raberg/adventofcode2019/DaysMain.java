1d657d801dc63e1f512899c6c0f10bd5e09b7336
Anders Råberg <anders.raberg@gmail.com>
2019-10-29 22:45:03 +0100

    Added basic build & src.

---
+++ src/main/java/se/anders_raberg/adventofcode2019/DaysMain.java
@@ -0,0 +1,13 @@
+package se.anders_raberg.adventofcode2019;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class DaysMain {
+    private final static Logger LOGGER = Logger.getLogger(DaysMain.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        LOGGER.info("Waiting for the start");
+        // Day1.run();
+    }
+}
