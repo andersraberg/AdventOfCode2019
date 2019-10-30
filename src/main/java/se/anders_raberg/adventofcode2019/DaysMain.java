e7d1d1c8a28db62a697ea38d0f6fcacb5317fd5c
Anders Råberg <anders.raberg@gmail.com>
2019-10-30 19:47:12 +0100

    Sonar fixes

--- src/main/java/se/anders_raberg/adventofcode2019/DaysMain.java
+++ src/main/java/se/anders_raberg/adventofcode2019/DaysMain.java
@@ -1,13 +1,11 @@
 package se.anders_raberg.adventofcode2019;
 
-import java.io.IOException;
 import java.util.logging.Logger;
 
 public class DaysMain {
-    private final static Logger LOGGER = Logger.getLogger(DaysMain.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DaysMain.class.getName());
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         LOGGER.info("Waiting for the start");
-        // Day1.run();
     }
 }
