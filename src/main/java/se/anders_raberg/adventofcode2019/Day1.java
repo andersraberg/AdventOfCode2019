a85f14288d0757959f1aafc084525f063bf2110e
Anders Råberg <anders.raberg@gmail.com>
2019-11-16 12:25:25 +0100

    Sonar fixes

--- src/main/java/se/anders_raberg/adventofcode2019/Day1.java
+++ src/main/java/se/anders_raberg/adventofcode2019/Day1.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 public class Day1 {
     private static final Logger LOGGER = Logger.getLogger(Day1.class.getName());
 
+    private Day1() {
+    }
+
     public static void run() throws IOException {
         List<Integer> changes = Files.readAllLines(Paths.get("inputs/input1.txt")).stream() //
                 .map(Integer::valueOf) //
@@ -19,6 +22,6 @@ public class Day1 {
         int sum = changes.stream().reduce(0, Integer::sum);
 
         // Results
-        LOGGER.info("Part 1: Sum: " + sum);
+        LOGGER.info(() -> String.format("Part 1: Sum: %s", sum));
     }
 }
